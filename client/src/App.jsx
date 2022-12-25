0b79ebc071f9faf4ea62becd61eaf96d1e624d2a
Subramanyarao11 <subramanya11rao@gmail.com>
2022-12-25 21:37:24 +0530

    first commit

---
+++ client/src/App.jsx
@@ -0,0 +1,10 @@
+function App() {
+
+  return (
+    <div>
+        <h1 className="text-center font-semibold">Let's Build a Full Stack Book Store Management System</h1>
+    </div>
+  )
+}
+
+export default App
